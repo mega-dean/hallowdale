b565162d06bf07dfacff5642f1142801d8586b3b
mega-dean <mega_dean@fastmail.com>
2023-09-17 20:48:54 -0600

    remove .mli files

--- src/platform.mli
+++
@@ -1,5 +0,0 @@
-open Types
-
-val start_rotating : platform -> game -> texture_cache -> unit
-val finish_rotating : platform -> game -> texture_cache -> unit
-val reset_rotation : platform -> game -> texture_cache -> unit
--- src/render.mli
+++
@@ -1,3 +0,0 @@
-open Types
-
-val tick : state -> state
